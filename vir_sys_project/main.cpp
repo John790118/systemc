ad3b5db34f432d14e2bc486b25b828494daf557b
John790118 <goodcross@outlook.com>
2021-10-14 11:47:45 +0800

    main.cpp

---
+++ vir_sys_project/main.cpp
@@ -0,0 +1,51 @@
+
+
+#include "stdio.h"
+#include <iostream>
+#include "packet_gen.h"
+#include "ingress_sch.h"
+#include "systemc.h"
+#include <memory>
+#include "comm_def.h"
+#include "comm_func.h"
+#include "pkt.h"
+#include "pkt_sender.h"
+#include "pkt_receiver.h"
+#include "pkt_switch.h"
+using namespace std;
+
+
+int sc_main(int argc, char *argv[])
+{
+   sc_signal<pkt> pkt_in0;
+   sc_signal<pkt> pkt_out0;
+
+   sc_clock clock1("CLOCK1", 10, SC_NS, 0.5, 0.0, SC_NS);
+
+   pkt_sender pkt_sender0("PKT_SENDER0");
+   pkt_sender0.pkt_out(pkt_in0);
+   pkt_sender0.CLK(clock1);
+
+   pkt_switch pkt_switch_top("PKT_SWTICH_TOP");
+   pkt_switch_top.clock1(clock1);
+   pkt_switch_top.in0(pkt_in0);
+   pkt_switch_top.out0(pkt_out0);
+
+   pkt_receiver pkt_receiver0("PKT_RECEIVER0");
+   pkt_receiver0.pkt_in(pkt_out0);
+
+   sc_start(0, SC_NS);
+
+   // create trace file
+   sc_trace_file *tf = sc_create_vcd_trace_file("Packet");
+   sc_trace(tf, clock1, "CLK1");
+   sc_trace(tf, pkt_in0, "PKT_IN0");
+   sc_trace(tf, pkt_out0, "PKT_OUT0");
+
+   sc_start(0.001,SC_MS);
+
+   // close trace file
+   sc_close_vcd_trace_file(tf);
+
+   return 0;
+}
